4246c77ebd19f4432b71045aa6f84d1367a4192c
Gereon Vincenti <g.vincenti@fz-juelich.de>
2023-10-07 16:58:03 +0200

    Simplify interface

--- src/gks/gks.rs
+++ src/gks/gks.rs
@@ -1,128 +1,113 @@
-use ::core::fmt::{self, Debug, Formatter};
 use ::core::ops::Deref;
 use ::std::sync::{Mutex, MutexGuard, TryLockError};
-use super::bindings;
+use super::bindings::gks::{gks_open_gks, gks_close_gks};
 
-pub const GKS: GksState = GksState::new();
+static GKS_MUTEX: Mutex<bool> = Mutex::new(false);
 
 pub struct Gks;
-pub struct GksState(Mutex<bool>);
-pub struct GksContext<'a>(MutexGuard<'a, bool>);
-
+pub struct GksGuard(MutexGuard<'static, bool>);
 pub enum GksTryLockError {
     Closed,
     WouldBlock,
 }
 
-impl Gks {
-    pub unsafe fn assume_context() -> Self {
-        Gks
+pub fn scope<R>(errfill: ::core::ffi::c_int, f: impl FnOnce(Gks) -> R) -> R {
+    struct DropGuard(MutexGuard<'static, bool>);
+    impl Drop for DropGuard {
+        fn drop(&mut self) {
+            if !*self.0 {
+                unsafe { gks_close_gks() }
+            }
+        }
     }
+    let guard = DropGuard(GKS_MUTEX.lock().unwrap_or_else(|e| e.into_inner()));
+    if !*guard.0 {
+        unsafe { gks_open_gks(errfill) }
+    }
+    f(Gks)
 }
 
-impl GksState {
-    const fn new() -> Self {
-        Self(Mutex::new(false))
+impl Gks {
+    pub unsafe fn assume_context() -> Gks {
+        Gks
     }
 
-    pub fn open(&self, errfill: ::core::ffi::c_int) -> Option<GksContext> {
-        let mut guard = self.0.lock().unwrap_or_else(|e| e.into_inner());
+    pub fn open(errfill: ::core::ffi::c_int) -> Option<GksGuard> {
+        let mut guard = GKS_MUTEX.lock().unwrap_or_else(|e| e.into_inner());
         match *guard {
             true => None,
             false => {
-                unsafe { bindings::gks::gks_open_gks(errfill) }
+                unsafe { gks_open_gks(errfill) }
                 *guard = true;
-                Some(GksContext::new(guard))
+                Some(GksGuard::new(guard))
             },
         }
     }
 
-    pub fn try_open(&self, errfill: ::core::ffi::c_int) -> Option<GksContext> {
-        let result = self.0.try_lock();
-        let mut guard = match result {
+    pub fn try_open(errfill: ::core::ffi::c_int) -> Option<GksGuard> {
+        let mut guard = match GKS_MUTEX.try_lock() {
             Ok(guard) if *guard => return None,
             Ok(guard) => guard,
             Err(TryLockError::Poisoned(err)) => err.into_inner(),
             Err(TryLockError::WouldBlock) => return None,
         };
-        unsafe { bindings::gks::gks_open_gks(errfill) }
+        unsafe { gks_open_gks(errfill) }
         *guard = true;
-        Some(GksContext::new(guard))
+        Some(GksGuard::new(guard))
     }
 
-    pub fn lock(&self) -> Option<GksContext> {
-        let guard = self.0.lock().unwrap_or_else(|e| e.into_inner());
+    pub fn lock() -> Option<GksGuard> {
+        let guard = GKS_MUTEX.lock().unwrap_or_else(|e| e.into_inner());
         match *guard {
-            true => Some(GksContext::new(guard)),
+            true => Some(GksGuard::new(guard)),
             false => None
         }
     }
 
-    pub fn try_lock(&self) -> Result<GksContext, GksTryLockError> {
-        match self.0.try_lock() {
-            Ok(guard) if *guard => Ok(GksContext::new(guard)),
+    pub fn try_lock() -> Result<GksGuard, GksTryLockError> {
+        match GKS_MUTEX.try_lock() {
+            Ok(guard) if *guard => Ok(GksGuard::new(guard)),
             Ok(_) => Err(GksTryLockError::Closed),
-            Err(TryLockError::Poisoned(err)) => Ok(GksContext::new(err.into_inner())),
+            Err(TryLockError::Poisoned(err)) => Ok(GksGuard::new(err.into_inner())),
             Err(TryLockError::WouldBlock) => Err(GksTryLockError::WouldBlock),
         }
     }
 
-    pub fn lock_and_open(&self, errfill: ::core::ffi::c_int) -> GksContext {
-        let mut guard = self.0.lock().unwrap_or_else(|e| e.into_inner());
+    pub fn opened(errfill: ::core::ffi::c_int) -> GksGuard {
+        let mut guard = GKS_MUTEX.lock().unwrap_or_else(|e| e.into_inner());
         if !*guard {
-            unsafe { bindings::gks::gks_open_gks(errfill) }
+            unsafe { gks_open_gks(errfill) }
             *guard = true;
         }
-        GksContext::new(guard)
+        GksGuard::new(guard)
     }
 
-    pub fn try_lock_and_open(&self, errfill: ::core::ffi::c_int) -> Option<GksContext> {
-        let mut guard = match self.0.try_lock() {
+    pub fn try_opened(errfill: ::core::ffi::c_int) -> Option<GksGuard> {
+        let mut guard = match GKS_MUTEX.try_lock() {
             Ok(guard) => guard,
             Err(TryLockError::Poisoned(err)) => err.into_inner(),
             Err(TryLockError::WouldBlock) => return None,
         };
         if !*guard {
-            unsafe { bindings::gks::gks_open_gks(errfill) }
+            unsafe { gks_open_gks(errfill) }
             *guard = true;
         }
-        Some(GksContext::new(guard))
+        Some(GksGuard::new(guard))
     }
 }
 
-impl Debug for GksState {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        f.debug_struct("GksState")
-         .field(
-            "initialized",
-            match self.try_lock() {
-                Ok(_) => &true,
-                Err(GksTryLockError::Closed) => &false,
-                Err(GksTryLockError::WouldBlock) => &"<locked>",
-            }
-         )
-         .finish()
-    }
-}
-
-impl Drop for GksState {
-    fn drop(&mut self) {
-        self.lock().map(GksContext::close);
-    }
-}
-
-impl<'a> GksContext<'a> {
-    fn new(guard: MutexGuard<'a, bool>) -> Self {
+impl GksGuard {
+    fn new(guard: MutexGuard<'static, bool>) -> Self {
         Self(guard)
     }
 
     pub fn close(mut self) {
-        unsafe { bindings::gks::gks_close_gks() }
+        unsafe { gks_close_gks() }
         *self.0 = false;
     }
 }
 
-impl Deref for GksContext<'_> {
+impl Deref for GksGuard {
     type Target = Gks;
 
     fn deref(&self) -> &Self::Target {
--- src/gks/mod.rs
+++ src/gks/mod.rs
@@ -1,4 +1,4 @@
 pub mod bindings;
 mod gks;
 
-pub use gks::{GKS, Gks};
+pub use gks::{Gks, scope};
