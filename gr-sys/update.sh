97db00c1882ef81c853e495a5742587c0bbfd6b8
Gereon Vincenti <g.vincenti@fz-juelich.de>
2023-10-10 15:53:34 +0200

    Automate updating header-files

---
+++ gr-sys/update.sh
@@ -0,0 +1,4 @@
+curl -o header/gks.h https://raw.githubusercontent.com/sciapp/gr/master/lib/gks/gks.h
+patch header/gks.h < patches/gks.patch
+curl -o header/gkscore.h https://raw.githubusercontent.com/sciapp/gr/master/lib/gks/gkscore.h
+patch header/gkscore.h < patches/gkscore.patch
