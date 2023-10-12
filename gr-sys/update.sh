cda1259755ca3ecff10fe190cf3ecd091147d0e7
Gereon Vincenti <g.vincenti@fz-juelich.de>
2023-10-12 20:47:38 +0200

    Add shebang

--- gr-sys/update.sh
+++ gr-sys/update.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 curl -o header/gks.h https://raw.githubusercontent.com/sciapp/gr/master/lib/gks/gks.h
 patch header/gks.h < patches/gks.patch
 curl -o header/gkscore.h https://raw.githubusercontent.com/sciapp/gr/master/lib/gks/gkscore.h
