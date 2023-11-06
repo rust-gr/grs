dff49238b508d25ca0b3a282084e0da55ccf5fe0
Gereon Vincenti <g.vincenti@fz-juelich.de>
2023-11-06 16:46:47 +0100

    Adapt example to changes

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,19 +1,16 @@
 use grs::gks::{ws::GksRegenerationFlag, Gks};
 use std::io::{stdin, Read};
 
-fn run() -> Option<()> {
-    let wkid = 1.try_into().ok()?;
-    let x = &[0., 1.];
-    let mut gks = Gks::open(0)?;
-    gks.open_ws(wkid, None, None)?;
-    let mut gks = gks.activate(wkid).ok()?;
-    gks.polyline(x.len(), x, x).ok()?;
-    let mut gks = gks.deactivate_all();
-    gks.ws(wkid)?.update(GksRegenerationFlag::Perform);
-    stdin().bytes().next();
-    Some(())
-}
-
 fn main() {
-    run().unwrap();
+    let wkid = 1;
+    let x = &[0., 1.];
+    let mut gks = Gks::open(0).unwrap();
+    gks.open_ws(wkid, None, None).unwrap();
+    let mut gks = gks.activate(wkid).unwrap();
+    gks.polyline(x.len(), x, x).unwrap();
+    let mut gks = gks.deactivate_all();
+    gks.ws(wkid)
+        .expect("workstation should still exist")
+        .update(GksRegenerationFlag::Perform);
+    stdin().bytes().next();
 }
