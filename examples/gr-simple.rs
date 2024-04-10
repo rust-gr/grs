806b2a9172ca54ccb1cfe14886496a6622390ee0
Gereon Vincenti <g.vincenti@fz-juelich.de>
2024-04-10 16:06:42 +0200

    Expand on GR-example

--- examples/gr-simple.rs
+++ examples/gr-simple.rs
@@ -1,8 +1,36 @@
 use grs::gr;
-use std::io::{stdin, Read};
+use std::{
+    ffi::{CStr, CString},
+    io::{stdin, Read},
+};
+
+struct Ticker {
+    invocations: usize,
+}
+impl Ticker {
+    const fn new() -> Self {
+        Ticker { invocations: 0 }
+    }
+
+    fn callback<'a, 'b>(&'a mut self, pos: (f64, f64), _s: &'b CStr, v: f64) {
+        self.invocations += 1;
+        let s = format!("{v} ({})", self.invocations);
+        let s = CString::new(s).unwrap();
+        gr::text(pos, s);
+    }
+}
 
 fn main() {
     let x = &[0., 1.];
+    let mut ticker = Ticker::new();
     gr::polyline(2, x, x).unwrap();
+    gr::axeslbl(
+        (0.1, 0.1),
+        (0.0, 0.0),
+        (Some(5), Some(10)),
+        0.02,
+        Some(&mut |p, s, v| ticker.callback(p, s, v)),
+        None,
+    );
     stdin().bytes().next();
 }
